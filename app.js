51dfb8d542023933dcb930a5dcbf7171f19e84f7
Ash-Cassandra <c.larcheveque@orange.fr>
2023-05-18 22:29:43 +0200

    ajout requete post

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -9,7 +11,12 @@ app.use((req, res, next) => {
     next();
   });
 
-app.use('/api/books', (req, res, next) => {
+app.post('/api/books', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({ message : 'livre créé '});
+});
+
+app.get('/api/books', (req, res, next) => {
     const book = [
         {
             "id": "1",
