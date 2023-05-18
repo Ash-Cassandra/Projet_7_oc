937c9d188d28b3d8eaf6ae072e232583b9d35cec
Ash-Cassandra <c.larcheveque@orange.fr>
2023-05-19 00:14:56 +0200

    ajout des controllers

---
+++ routes/book.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const bookCtrl = require('../controllers/book');
+
+router.post('/', bookCtrl.createBook);
+router.put('/:id', bookCtrl.modifyBook);
+router.delete('/:id', bookCtrl.deleteBook);
+router.get('/:id', bookCtrl.findOneBook);
+router.get('/', bookCtrl.findAllBook);
+
+module.exports = router;
