b1e80baf97c9600c2415ad2d49662e0f7b64ea78
Ash-Cassandra <c.larcheveque@orange.fr>
2023-05-19 13:17:37 +0200

    ajout de multer

--- routes/book.js
+++ routes/book.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const auth = require.Router();
+const auth = require('../middleware/auth');
 const router = express.Router();
 
 const bookCtrl = require('../controllers/book');
 
 router.post('/', auth, bookCtrl.createBook);
-router.put('/:id', auth,  bookCtrl.modifyBook);
+router.put('/:id', auth, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 router.get('/:id', auth, bookCtrl.findOneBook);
 router.get('/', auth, bookCtrl.findAllBook);
